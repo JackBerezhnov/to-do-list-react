0e473e437e7308d1ba6c1a44954ec4bbe8d7977d
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-04-04 21:52:58 -0700

    Create Item

---
+++ src/components/item-list/ItemList.js
@@ -0,0 +1,13 @@
+const ItemList = (props) => {
+    return (
+        <div>
+        
+        {console.log("Data", props)}
+            <ul>
+                <li>{props.items}</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ItemList;
--- src/components/to-do-list/ToDoList.js
+++ src/components/to-do-list/ToDoList.js
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 import Input from '../input/Input';
+import ItemList from '../item-list/ItemList';
 
 function ToDoList() {
     const [userInput, setUserInput] = useState("");
+    const [toDoItems, setToDoItems] = useState([""]);
 
     function handleChange(e) {
         setUserInput(e.target.value);
+    }
+
+    function submitUserData() {
+        setToDoItems(userInput);
     }
     
     return(
         <div>
             <h1>To Do List!</h1>
             <Input onChange={handleChange}/>
+            <button value="Send" className="btn success" onClick={submitUserData}>Add</button>
+            <ItemList items={toDoItems}/>
             <p>{userInput}</p>
         </div>
     );
