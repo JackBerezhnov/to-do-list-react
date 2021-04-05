2a8fb8677f0115158529ea66ad5e1e1bf3f4e16f
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-04-04 21:25:25 -0700

    Setup Input for the app

---
+++ src/components/input/Input.js
@@ -0,0 +1,18 @@
+import './Input.css';
+
+const Input = (props) => {
+    return (
+        <div>
+            {console.log(props)}
+            <input 
+                type="text"
+                name="toDo"
+                id="toDoInput"
+                placeholder="Enter a thing what you need to do..."
+                onChange={e => props.onChange(e)}
+            />
+        </div>
+    );
+}
+
+export default Input;
--- src/components/to-do-list/ToDoList.js
+++ src/components/to-do-list/ToDoList.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import Input from '../input/Input';
+
 function ToDoList() {
+    const [userInput, setUserInput] = useState("");
+
+    function handleChange(e) {
+        setUserInput(e.target.value);
+    }
+    
     return(
         <div>
             <h1>To Do List!</h1>
+            <Input onChange={handleChange}/>
+            <p>{userInput}</p>
         </div>
     );
 }
